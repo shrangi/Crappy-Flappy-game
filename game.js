fdc999f6593f62d08b74595a3388a8e442849da3
shrangi <35887384+shrangi@users.noreply.github.com>
2018-06-21 19:17:28 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
 var cvs = document.getElementById("canvas") ;
+var ctx = document.getContext("2d");
+
+// load images
+
+var bird = new Image();
+var bg= new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src = "images/bird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+piepNorth.src = "images/pipeNorth.png" ;
+pipeSouth.src = "images/piepSouth.png" ;
+
+// some variables
+
+var gap = 80;
+var constant = pipeNorth.height + gap ;
+
+// drw images 
+
+function draw()
+{ 
+    ctx.drawImage(bg,0,0) ;
+    ctx.drawImage(pipeNorth,100,0) ;
+    ctx.drawImage(pipeSouth,100, 0+constant) ;
+    
+  
+
+}
